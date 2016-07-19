94c67fdc652667fe14e3b66481ff4482bb83a1bf
desmond1993 <d.o.ellsworth@spartans.nsu.edu>
2016-07-19 16:45:51 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,117 @@
+<!--<!DOCTYPE html>-->
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="base.css">
+</head>
+<body>
+	<h1>Desmond Ellsworth</h1>
+	<h3>31133 Darden Street, Franklin, VA 23851 | (434) 594-4309 | d.o.ellsworth@spartans.nsu.edu</h3>
+
+	<div id="section">
+		Education
+	</div>
+	<div id="info">
+		Liberty University – Lynchburg, VA<br>
+		Master of Science in Information Technology: Software Design & Management Cognate&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;May 2017
+		<p><p><p>
+			Norfolk State University – Norfolk, VA<br>
+			Bachelor of Science: Computer Science  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;May 2016
+		</div>
+		<div id="brake">
+		</div>
+		<div id="section">
+			Research
+		</div>
+		<div id="info">
+			Web Developer - Independent Study, Norfolk State University Computer Science Department<br>
+			Norfolk, VA &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;January 2016– May 2016
+			<ul>
+				<li>Developed a virtual application for students to apply for STARS summer research.</li>
+				<li>Used Ruby on Rails to create the application.</li>
+				<li>Bootstrap, JavaScript, and CSS were used to format the application.</li>
+			</ul>
+
+			Senior Thesis Researcher, Norfolk State University Computer Science Department<br>
+			Norfolk, VA &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;September 2015 – April 2016
+			<ul>
+				<li>Researched "Cluster computing and Parallel programming" and presented the simulation of the N-Body problem.</li>
+				<li>Built a nine node cluster Linux based.</li>
+				<li>Wrote a sequential and parallel version of the N-Body problem in C++, MPI, and OpenGL.</li>
+				<li>Tested the execution speed and time of the cluster, MacBook Pro, and Raspberry Pi cluster.</li>
+			</ul>
+			Researcher, Norfolk State University STARS Undergraduate research<br>
+			Norfolk, VA &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;May 2014 – July 2014
+			<ul>
+				<li>Researched visual analytics.</li>
+				<li>Created data sets in CSV and shapefiles to be used in WEAVE.</li>
+				<li>Imported data into WEAVE and selected the best manner to visualize using tools provided by WEAVE. </li>
+				<li>Presented research in the WHBR-LSAMP undergraduate symposium poster presentation.</li>
+			</ul>
+		</div>
+		<div id="brake">
+		</div>
+		<div id="brake">
+		</div>
+		<div id="brake">
+		</div>
+		<div id="section">
+			Work History 
+		</div>
+		<div id="info">
+			Lab assistant, Norfolk State University Office of Information Technology<br>
+			Norfolk, VA &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;January 2013 – December 2015
+			<ul>
+				<li>Monitored computer labs.</li>
+				<li>Installed and updated software and computers within the labs.</li>
+				<li>Provided first level troubleshooting of processes to students. </li>
+			</ul>
+			Office Assistance, Norfolk State University Office of Faculty Development<br>
+			Norfolk, VA &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;May 2015 – September 2015
+			<ul>
+				<li>Assisted in creating forms (travel, doctoral tuition assistance, and release time) for faculty members to complete.</li>
+				<li>Updated the website for Faculty Development using Percussion and HTML and CSS.</li>
+			</ul>
+		</div>
+		<div id="brake">
+		</div>
+		<div id="brake2">
+		</div>
+		<div id="brake2">
+		</div>
+		<div id="brake2">
+		</div>
+		<div id="section">
+			Programming Experience 
+		</div>
+		<div id="info">
+			<img src="img/skills.png" width="800px" height="300px">
+		</div>
+		<div id="brake">
+		</div>
+		<div id="brake">
+		</div>
+
+		<div id="section">
+			Accomplishments and Awards 
+		</div>
+		<div id="info">
+			Dominion Enterprises "Hack U – Coding Student Life", Norfolk, VA
+			<ul>
+				<li>1st – 2015 <a href="http://www.prweb.com/releases/2015/02/prweb12533783.htm"> Read About it here!!</a>&nbsp;&nbsp;&nbsp; <a href="https://github.com/dehackathon/NSU-Gold-HackU3">Here is the code for 2015 to check out.</a></li>
+				<li>3rd – 2016<a href="https://github.com/desmond1993/NSUHAck2016">Here is the code for 2015 to check out.</a></li>
+			</ul>
+
+			Norfolk State University Computer Science department research symposium 2016
+			<ul>
+				<li>"Cluster Computing and Parallel programming"</li>
+			</ul>
+		</div>
+	</div>
+	<div id="brake">
+	</div>
+	<div id="brake2">
+	</div>
+</body>
+
+</html>
+
---
+++ nav.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<style>
+<link rel="stylesheet" type="text/css" href="nav.css">
+</style>
+</head>
+<body>
+<ul>
+	<li><a href = "#home">Home</a></li>
+	<li><a href = "#home">Education</a></li>
+	<li><a href = "#home">Research</a></li>
+	<li><a href = "#home">Class Projects</a></li>
+	<li><a href = "#home">Work History</a></li>
+	<li><a href = "#home">Skills</a></li>
+	<li><a href = "#home">Accomplishments</a></li>
+	<li><a href = "#home">About Me</a></li>
+	<li><a href = "#home">Contact Me</a></li>
+</ul>
+</body>
+</html>
